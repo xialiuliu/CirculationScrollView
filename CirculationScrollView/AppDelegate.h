a563daddb70a7cd95b144b2f823780eb2a6a1b56
lcy <1193926130@qq.com>
2015-03-17 17:29:01 +0800

    Initial Commit

---
+++ CirculationScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CirculationScrollView
+//
+//  Created by liucaiyang on 15/3/17.
+//  Copyright (c) 2015年 lcy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
